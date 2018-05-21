dd59b54e6fc6a32cda3c2b88e41d4e91bde9baaa
Simon Valentin <valentin.simon@viacesi.fr>
2018-05-21 11:18:18 +0200

    Fix commit

--- app/src/main/java/com/example/valentin/zaptowork/Settings/SettingsFragment.java
+++ app/src/main/java/com/example/valentin/zaptowork/Settings/SettingsFragment.java
@@ -6,8 +6,14 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.valentin.zaptowork.R;
+
+import java.io.IOException;
+import java.net.InetAddress;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -15,6 +21,9 @@ public class SettingsFragment extends Fragment implements SettingsContract.View 
 
     SettingsContract.Presenter mPresenter;
 
+    Button checkConnectivityButton;
+
+    EditText addressServer;
 
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
@@ -23,7 +32,30 @@ public class SettingsFragment extends Fragment implements SettingsContract.View 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.content_setttings, container, false);
+        View root = inflater.inflate(R.layout.content_setttings, container, false);
+
+        addressServer = (EditText) root.findViewById(R.id.editText);
+
+        checkConnectivityButton = (Button) root.findViewById(R.id.checkCo);
+
+        checkConnectivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tmb = addressServer.getText().toString();
+                try {
+
+                    if( InetAddress.getByName(tmb).isReachable(10000))
+                        Toast.makeText(getContext(),"Working",Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getContext(),"Pas Disponible",Toast.LENGTH_LONG).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        return root;
     }
 
     @Override
